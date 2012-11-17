bdbea83c188a181e48c3b21d0472d7f737c1f950
John Robinson <soco@uw.edu>
2012-11-17 01:00:16 -0800

    updated the model

--- main/models.py
+++ main/models.py
@@ -34,7 +34,7 @@ class Courses(models.Model):
     )
 
 
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     number = models.IntegerField(db_column='Number') # Field name made lowercase.
     name = models.CharField(max_length=384, db_column='Name', blank=True) # Field name made lowercase.
     comment = models.CharField(max_length=768, db_column='Comment', blank=True) # Field name made lowercase.
@@ -54,13 +54,13 @@ class Instructors(models.Model):
 
 class Ratings(models.Model):
     id = models.AutoField(primary_key=True, db_column='id') # Field name made lowercase.
-    coursewhole = models.DecimalField(decimal_places=0, null=True, max_digits=9, db_column='CourseWhole') # Field name made lowercase.
-    coursecontent = models.DecimalField(decimal_places=0, null=True, max_digits=9, db_column='CourseContent', blank=True) # Field name made lowercase.
-    instructoreffectiveness = models.DecimalField(decimal_places=0, null=True, max_digits=9, db_column='InstructorEffectiveness', blank=True) # Field name made lowercase.
-    instructorcontribution = models.DecimalField(decimal_places=0, null=True, max_digits=9, db_column='InstructorContribution', blank=True) # Field name made lowercase.
-    instructorinterest = models.DecimalField(decimal_places=0, null=True, max_digits=9, db_column='InstructorInterest', blank=True) # Field name made lowercase.
-    amountlearned = models.DecimalField(decimal_places=0, null=True, max_digits=9, db_column='AmountLearned', blank=True) # Field name made lowercase.
-    grading = models.DecimalField(decimal_places=0, null=True, max_digits=9, db_column='Grading', blank=True) # Field name made lowercase.
+    coursewhole = models.DecimalField(decimal_places=4, null=True, max_digits=8, db_column='CourseWhole') # Field name made lowercase.
+    coursecontent = models.DecimalField(decimal_places=4, null=True, max_digits=8, db_column='CourseContent', blank=True) # Field name made lowercase.
+    instructoreffectiveness = models.DecimalField(decimal_places=4, null=True, max_digits=8, db_column='InstructorEffectiveness', blank=True) # Field name made lowercase.
+    instructorcontribution = models.DecimalField(decimal_places=4, null=True, max_digits=8, db_column='InstructorContribution', blank=True) # Field name made lowercase.
+    instructorinterest = models.DecimalField(decimal_places=4, null=True, max_digits=8, db_column='InstructorInterest', blank=True) # Field name made lowercase.
+    amountlearned = models.DecimalField(decimal_places=4, null=True, max_digits=8, db_column='AmountLearned', blank=True) # Field name made lowercase.
+    grading = models.DecimalField(decimal_places=4, null=True, max_digits=8, db_column='Grading', blank=True) # Field name made lowercase.
     numsurveyed = models.IntegerField(db_column='NumSurveyed') # Field name made lowercase.
     numenrolled = models.IntegerField(db_column='NumEnrolled') # Field name made lowercase.
     class Meta:
@@ -68,12 +68,12 @@ class Ratings(models.Model):
 
 
 class Instances(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     year = models.IntegerField(db_column='Year') # Field name made lowercase.
     quarter = models.CharField(max_length=6, db_column='Quarter') # Field name made lowercase.
     section = models.CharField(max_length=12, db_column='Section', blank=True) # Field name made lowercase.
     idinstructor = models.ForeignKey(Instructors, db_column='idInstructor') # Field name made lowercase.
-    idcourses = models.ForeignKey(Courses, db_column='idCourses') # Field name made lowercase.
+    idcourse = models.ForeignKey(Courses, db_column='idCourses') # Field name made lowercase.
     idratings = models.ForeignKey(Ratings, db_column='idRatings') # Field name made lowercase.
     instructortitle = models.CharField(max_length=192, db_column='InstructorTitle') # Field name made lowercase.
     class Meta:
