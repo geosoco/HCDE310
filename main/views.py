4855368e439851ac1a97591288f1bba9ae8ff321
John Robinson <soco@uw.edu>
2012-11-18 13:35:02 -0800

    update views

--- main/views.py
+++ main/views.py
@@ -1,6 +1,11 @@
 # Create your views here.
 from django.template import Context, loader
 from django.http import HttpResponse
+from django.shortcuts import *
 
 def index(request):
     return HttpResponse('test')
+
+
+def test(request):
+	return render_to_response('test.html', {'title': 'Super Fantastic', 'topic': 'Fun!'})
