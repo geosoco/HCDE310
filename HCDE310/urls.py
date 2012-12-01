318cf715d16e4f2b4979f632115538b6d132fc8b
John Robinson <soco@uw.edu>
2012-12-01 14:14:32 -0800

    admin urls

--- HCDE310/urls.py
+++ HCDE310/urls.py
@@ -2,15 +2,15 @@ from django.conf.urls import patterns, include, url
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'main.views.index'),
     url(r'^test/$', 'main.views.test'),
     url(r'^instructor/(?P<id>\d+)$', 'main.views.instructor'),
     url(r'^dept/(?P<id>\d+)$', 'main.views.department'),
-    url(r'^course/(?P<id>\d+)$', 'main.views.course')
+    url(r'^course/(?P<id>\d+)$', 'main.views.course'),
     # Examples:
     # url(r'^$', 'HCDE310.views.home', name='home'),
     # url(r'^HCDE310/', include('HCDE310.foo.urls')),
@@ -19,7 +19,7 @@ urlpatterns = patterns('',
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^admin/', include(admin.site.urls)),
 )
 
 urlpatterns += staticfiles_urlpatterns()
