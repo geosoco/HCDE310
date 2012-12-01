d6802f0d7aec81b91484a8eef2c5c976d6075ae5
John Robinson <soco@uw.edu>
2012-12-01 12:34:34 -0800

    add the import courses custom command

---
+++ main/management/commands/import_courses.py
@@ -0,0 +1,114 @@
+from django.core.management.base import BaseCommand, CommandError
+import django.db
+import csv
+import sys
+import os
+from main.models import Departments, Courses, Instances, Ratings, Instructors
+import pprint
+
+
+def GetDept(abbr):
+	try:
+		d = Departments.objects.get(abbreviation = abbr)
+		return d
+	except Departments.DoesNotExist, e:
+		return None
+
+def AddCourse(dept, number, name, comment = "", descr = "", genedreqs = 0, firstyear = None, firstquarter = None, lastyear = None, lastquarter = None, mincredits = None, maxcredits = None):
+	try:
+		c = Courses.objects.get(iddepartment = dept, number = long(number))
+		return c
+	except Courses.DoesNotExist, e:
+		try:
+			c = Courses(name=name, number=long(number), iddepartment=dept, comment = comment, description = descr)
+			c.genedreqs = genedreqs
+			c.firstyear = firstyear
+			c.firstquarter = firstquarter
+			c.lastyear = lastyear
+			c.lastquarter = lastquarter
+			c.mincredits = mincredits
+			c.maxcredits = maxcredits
+			c.save()
+			return c
+		except Exception, e:
+			print "exception!"
+			print e
+			return None
+	except Exception, e:
+		print "AddCourse - exception: ", e
+
+
+
+quarters = {
+	'autumn' : 'AU',
+	'winter' : 'WI',
+	'spring' : 'SP',
+	'summer' : 'SU'
+}
+
+
+
+class Command(BaseCommand):
+	args = '<directory>'
+	help = 'Imports courses. \nUsage: import_courses %s'
+
+	depts = {}
+	courses = {}
+	instructors = {}
+	coursetypes = {}
+	quarters = set()
+
+
+	def handle(self, *args, **options):
+		#print "Version: %s"%django.get_version()
+		if args is None or len(args) == 0:
+			raise CommandError('must specify directory')
+		#print "opening %s"%args[0]
+		subdir = args[0]
+
+		c = Courses()
+
+		for root, dirs, files in os.walk(subdir):
+			for name in files:
+				filename = os.path.join(root,name)
+
+				self.stdout.write('opening: %s\n'%filename)
+
+				abbr = os.path.splitext(os.path.basename(filename))[0]
+				self.stdout.write(abbr + '\n')
+
+				curriculum = GetDept(abbr)
+				if curriculum is None:
+					self.stderr.write('Couldn\'t find the curriculum' )
+					continue
+
+				with open(filename,'rU') as csvfile:
+					csvreader = csv.DictReader(csvfile)
+
+					#
+					for row in csvreader:
+						#print row
+						genedreqs = c.convertGenEdReqsToInt(row['GE_NW'], row['GE_VLPA'], row['GE_IS'], row['GE_W'], row['GE_EC'], row['GE_QSR'])
+
+						mincr = float(row['MinTermCredit'])
+						maxcr = float(row['MaxTermCredit']) 
+						maxcr = mincr if maxcr < maxcr else maxcr
+
+						AddCourse(curriculum , 
+							int(row['CourseNumber']), 
+							row['CourseTitleLong'], 
+							row['CourseComment'], 
+							row['CourseDescription'],
+							genedreqs, 
+							int(row['FirstYear']),
+							quarters.get(row['FirstQuarter']),
+							int(row['LastYear']),
+							quarters.get(row['LastQuarter']),
+							mincr,
+							maxcr
+							 )
+
+
+
+
+
