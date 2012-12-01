78bdcd64980645c797c5015b37a8c7621af24004
John Robinson <soco@uw.edu>
2012-12-01 12:34:12 -0800

    used for the admin console

---
+++ main/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from main.models import Departments, Courses, Instructors, Ratings, Instances, Meetings
+
+admin.site.register(Departments)
+admin.site.register(Courses)
+admin.site.register(Instructors)
+admin.site.register(Ratings)
+admin.site.register(Instances)
+admin.site.register(Meetings)
