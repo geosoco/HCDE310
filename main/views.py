8437fd59b4b32d8b7929f2c328db29f8dd3dbcc8
Megan Torkildson <mtorkild@uw.edu>
2012-12-02 15:50:37 -0800

    updated css and js files

--- main/views.py
+++ main/views.py
@@ -7,32 +7,32 @@ import simplejson as json
 
 
 def index(request):
-    return HttpResponse('test')
+    return render(request, 'index.html')
 
 
 def test(request):
-	return render_to_response('test.html', {'title': 'Super Fantastic', 'topic': 'Fun!'})
+	return render(request, 'test.html', {'title': 'Super Fantastic', 'topic': 'Fun!'})
 
 
 def instructor(request, id=1):
 	i = get_object_or_404(Instructor, pk=long(id))
 	c = Section.objects.filter(idinstructor=i)
-	return render_to_response('instructor.html', {"instructor" : i, "courseinstances" : c})
+	return render(request, 'instructor.html', {"instructor" : i, "courseinstances" : c})
 
 
 def department(request, id):
 	d = get_object_or_404(Curriculum, pk=long(id))
 	c = Course.objects.filter(idcurriculum=d)
-	return render_to_response('department.html', {"department" : d, "courses" : c})
+	return render(request, 'department.html', {"department" : d, "courses" : c})
 
 
 def course(request, id):
 	c = get_object_or_404(Course, pk=long(id))
 	inst = Section.objects.filter(idcourse=c)
-	return render_to_response('course.html', {"course" : c, "instances" : inst})
+	return render(request, 'course.html', {"course" : c, "instances" : inst})
 
 def course_readable(request, curriculum, num):
 	cur = get_object_or_404(Curriculum, abbreviation=curriculum)
 	c = get_object_or_404(Course, idcurriculum=cur, number=num)
 	inst = Section.objects.filter(idcourse=c)
-	return render_to_response('course.html', {"course" : c, "instances" : inst})	
+	return render(request, 'course.html', {"course" : c, "instances" : inst})	
